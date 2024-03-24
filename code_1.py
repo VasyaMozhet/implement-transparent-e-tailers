d912525e9e2f778a57ca035b2eca8dcab5e99931
VasyaMozhet <158567931+VasyaMozhet@users.noreply.github.com>
2024-03-25 00:09:16 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+
+
+    return data
+
+    main()
+def generate_random_data():
+def main():
+if __name__ == "__main__":
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
